a951c01f610c13bec74bfe94df89278c7f02b58a
Gatom62 <20240046@ricaldone.edu.sv>
2024-07-23 18:40:21 -0600

    Unicamente puse el codigo para poder acceder a una base de datos en linea

--- Modelo/dbContext.cs
+++ Modelo/dbContext.cs
@@ -13,12 +13,20 @@ namespace AgroServicios.Modelo
         {
             try
             {
+                //Conextor a base de datos local
                 //string server = "DESKTOP-QR03KRF";
-                string server = "FELIPE\\SQLEXPRESS";
-                string database = "Base_de_datos_Agro";
-                SqlConnection conexion = new SqlConnection("Server =" + server +
-                                                                 "; DataBase = " + database +
-                                                                 "; Integrated Security = true;");
+                //string server = "FELIPE\\SQLEXPRESS";
+                //string database = "Base_de_datos_Agro";
+                //SqlConnection conexion = new SqlConnection("Server =" + server +
+                //                                                 "; DataBase = " + database +
+                //                                                 "; Integrated Security = true;");
+                
+                //Conector a base de datos en linea
+                string server = "SQL8006.site4now.net";
+                string database = "db_aab115_siasbase";
+                string userId = "db_aab115_siasbase_admin";
+                string Password = "MichI#12@3";
+                SqlConnection conexion = new SqlConnection($"Server = {server}; DataBase = {database}; User Id = {userId}; Password = {Password}");
                 conexion.Open();
                 return conexion;
             }

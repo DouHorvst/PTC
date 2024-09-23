e8c9f5d26bb77ece1a3a6117c5c9d14c5b8e27ec
Gatom62 <20240046@ricaldone.edu.sv>
2024-09-22 22:56:49 -0600

    SAIA
    Nose

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+using AgroServicios.Vista.Login;
+using AgroServicios.Vista.Cuentas;
+using AgroServicios.Modelo.DAO;
+
+namespace AgroServicios
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // Verificación de Primer Uso
+            DAOLogin Verificacion = new DAOLogin();
+
+            if (Verificacion.PrimerUso() == true)
+            {
+                Application.Run(new VistaLogin());
+            }
+            else
+            {
+                // Crear un formulario CreateUser
+                CreateUser createUserForm = new CreateUser(accion: 1);
+
+                // Mostrar CreateUser como un formulario modal
+                createUserForm.ShowDialog();
+
+                // Una vez que CreateUser se cierra, abrir VistaLogin
+                Application.Run(new VistaLogin());
+            }
+        }
+    }
+
+}
